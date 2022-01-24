f3aacf35c94ea5618484fc586a465a103b4edf83
Pirashanth Theventhiran <pirashanth.theventhiran@hygienicecho.com>
2022-01-23 19:03:13 -0500

    week 2 commits built the days and days list

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import className from 'classnames';
+import 'components/InterviewerListItem.scss';
+
+export default function InterviewerListItem(props) {
+  const styleClassInterviewer = className('interviewers__item', {
+    'interviewers__item--selected': props.selected,
+  });
+  return (
+    <li className={styleClassInterviewer} onClick={props.setInterviewer}>
+        <img 
+            className='interviewers__item-image' 
+            src={props.avatar} 
+            alt={props.name} 
+        />
+        {props.selected && props.name}
+    </li>
+  );
+}
